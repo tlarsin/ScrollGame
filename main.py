8af9c4e20e4e38fe4e3201e059a49fd0e3b52065
Tristan Larsin <larsi002@d.umn.edu>
2017-01-20 19:24:08 -0600

    Initial commit of files. Main.py and Run.kv have been set with the basic outline. Sprite.bmp was also included as a sprite for future use.

---
+++ main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class SpriteCharacter(Widget):
+    pass
+
+class RunGame(Widget):
+    pass
+
+class RunApp(App):
+    def build(self):
+        pass
+
+if __name__ == "__main__":
+    RunApp().run()
