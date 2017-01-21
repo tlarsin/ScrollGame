63fca6868c98be34e74a40412b3f3c3cda5d4ecd
Tristan Larsin <larsi002@d.umn.edu>
2017-01-20 19:48:53 -0600

    Added basic methods for keyboard requests for future use when character has been created. Updated run.kv file with character information (sprite image, position, and size). Also added the character information to the <RunGame> class inside of run.kv

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.properties import ListProperty, NumericProperty, ReferenceListProperty,\
+    ObjectProperty
 
 class SpriteCharacter(Widget):
     pass
 
 class RunGame(Widget):
-    pass
+    sprite = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(RunGame, self).__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        return True
 
 class RunApp(App):
     def build(self):
-        pass
+        game = RunGame()
+        return game
 
 if __name__ == "__main__":
     RunApp().run()
